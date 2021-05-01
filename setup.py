a0d164cbbc752ee65092bb490891d86a157d1c1f
Sarmad Gulzar <sarmadgulzar@outlook.com>
2021-05-01 00:58:17 +0000

    Fixed root directory of the package

--- setup.py
+++ setup.py
@@ -5,14 +5,15 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="firex",
-    version="0.0.1",
+    version="0.0.2",
     author="Sarmad Gulzar",
     author_email="sarmadgulzar@hotmail.com",
     description="Firebase with Python",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/sarmadgulzar/firex",
-    packages=setuptools.find_packages(),
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
